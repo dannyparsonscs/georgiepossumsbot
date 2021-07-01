fa6878a3373eb5efaa00bbc557db233b76a29066
House <34289390+thhpii@users.noreply.github.com>
2021-07-01 11:26:19 -0400

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key: "QDlum20M9OHAjpsnAueXujQPt",
-    consumer_secret: "EnIWG4bijp6fomcz4gpHxIQ7SF6x76NjFRPeFeVYqOmtNIUb1v",
-    access_token: "1410387622183194624-WjaVSlRd194SaLoTRnIwXbP6U3HCXS",
-    access_token_secret: "G9T5Aypw49h5ASxcAPJ9RagET27p3ORJRnb3I08VxC5gD"
-}
